1b47d9f67f4a47affe19b2793d97e307e518e77c
Gustavo Viegas <gustavoviegas231914@gmail.com>
2021-03-17 16:36:44 -0300

    :white_check_mark: Teste de Nodemon
    Não importante

--- App.js
+++ App.js
@@ -6,9 +6,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/Aula01', (req, res) => {
-    res.send('<h1>Bem-vindo... Teste Dois!</h1>')
+app.get('/Teste01', (req, res) => {
+    res.send('<h1>Bem-vindo... Primeiro Teste!</h1>')
 })
+
+/*Nodemon pacote que reinicializa automaticamente o servidor interno sempre que os arquivos
+forem alterados*/
+app.get('/Teste02', (req, res) => {
+    res.send('<h1>Teste do Nodemon!</h1>')
+})
+
 
 app.listen(port, () => {
   console.log(`Servidor Rodando -> http://localhost:${port}`)
